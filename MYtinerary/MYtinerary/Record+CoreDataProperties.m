d0823b6c9bf2281210ab2f72197b34f8490307d2
sungkim3 <sng.kim103@gmail.com>
2016-08-04 14:43:45 -0700

    set up first stage of Core Data entity models Record and Itinerary

---
+++ MYtinerary/MYtinerary/Record+CoreDataProperties.m
@@ -0,0 +1,26 @@
+//
+//  Record+CoreDataProperties.m
+//  MYtinerary
+//
+//  Created by Sung Kim on 8/4/16.
+//  Copyright © 2016 Sung Kim. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Record+CoreDataProperties.h"
+
+@implementation Record (CoreDataProperties)
+
+@dynamic localImageURL;
+@dynamic parseImageURL;
+@dynamic parseThumbnailURL;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic date;
+@dynamic title;
+@dynamic comments;
+@dynamic itinerary;
+
+@end
