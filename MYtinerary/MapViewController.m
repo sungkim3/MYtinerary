c4e0da7131ee9fcc07c4801b3560dbbec2817318
Jess Malesh <jamalesh84@gmail.com>
2016-08-09 10:04:24 -0700

    logo image updated

--- MYtinerary/MapViewController.m
+++ MYtinerary/MapViewController.m
@@ -193,7 +193,7 @@ typedef void(^imageCompletion)(UIImage *image);
 
 #pragma mark - PFSignUpViewControllerDelegate
 
-- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+- (void)signUpViewController:(CustomLoginViewController *)signUpController didSignUpUser:(PFUser *)user {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
