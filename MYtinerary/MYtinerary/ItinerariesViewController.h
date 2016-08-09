fee4c2b13d0b68d70b3af3d11cf8e9271632698e
sungkim3 <sng.kim103@gmail.com>
2016-08-08 19:54:49 -0700

    starts itineraryviewcontroller to fetch from core data to populate existing itineraries in core data context

---
+++ MYtinerary/MYtinerary/ItinerariesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItinerariesViewController.h
+//  MYtinerary
+//
+//  Created by Sung Kim on 8/8/16.
+//  Copyright © 2016 Sung Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItinerariesViewController : UIViewController
+
+@end
